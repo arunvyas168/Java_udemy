05d35a9dba784c87d8b1e0db36d134b3058e33f6
arunvyas168 <arunvyas27@gmail.com>
2016-04-17 12:52:42 -0700

    Example of generic thst we use in ArrayList autoboxing

---
+++ Udemy/Generics/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> items = new ArrayList<>();
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        //items.add("Arun");
+        items.add(5);
+        items.add(6);
+        printDoubled(items);
+
+    }
+    private static void printDoubled(ArrayList<Integer> list){
+        for (int i : list){
+            System.out.println((i *2));
+        }
+    }
+}
