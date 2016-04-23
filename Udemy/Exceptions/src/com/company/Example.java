95862878d640b1b2a73ee216a1d348a3cd53b503
arunvyas168 <arunvyas27@gmail.com>
2016-04-22 18:32:40 -0700

    Multiple exception

--- Udemy/Exceptions/src/com/company/Example.java
+++ Udemy/Exceptions/src/com/company/Example.java
@@ -9,23 +9,26 @@ import java.util.Scanner;
  */
 public class Example {
     public static void main(String[] args) {
-        int result  = divide();
-        System.out.println("The result is: "+ result);
+        try {
+            int result  = divide();
+            System.out.println("The result is: "+ result);
+        } catch (ArithmeticException | NoSuchElementException e){  //Multiple exception catch
+            System.out.println(e);
+            System.out.println("Unable to perform division");
+        }
     }
 
-    public static int divide(){
-        int x,y;
-        try {
+    public static int divide() {
+        int x, y;
+//        try {
             x = getInt();
             y = getInt();
-        } catch (NoSuchElementException e){
-           throw new ArithmeticException("No suitable Input");
-        }
-        try {
-            return x/y;
-        } catch (ArithmeticException e){
-            throw new ArithmeticException("Atempt to devide by Zero");
-        }
+            return x / y;
+//        } catch (NoSuchElementException e) {
+//            throw new ArithmeticException("No suitable Input");
+//        } catch (ArithmeticException e) {
+//            throw new ArithmeticException("Atempt to devide by Zero");
+//        }
     }
 
     public static int getInt(){
